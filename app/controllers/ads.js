db5e1d06f6edf5e5a2914512dabed6dfc525da0f
PierreGUI <pierre.vdev@gmail.com>
2015-04-15 22:42:58 +0200

    Use a single instance of the DFP module in Ads

--- app/controllers/ads.js
+++ app/controllers/ads.js
@@ -1,9 +1,11 @@
-var TAG = "DEMO";
-// List available ads in the view
+var TAG = "DEMO",
+    DFP = require("ti.dfp");
+
+// List ads to be created
 var banners = {
-    default: "defaultBanner",
-    sizes: "sizesBanner",
-    smart: "smartBanner"
+    default: null,
+    smart: null,
+    sizes: null
 };
 
 // Construct
@@ -11,6 +13,49 @@ var banners = {
     Ti.API.info(TAG, "Construct ads page");
     $.version.text += Ti.App.version;
     resetButton();
+
+    // Instantiate DFP views
+    banners.default = DFP.createView({
+        top: 20,
+        width: 320,
+        height: 50,
+        adWidth: 320,
+        adHeight: 50,
+        adUnitId: "/6236286/defaultBanner",
+        location: {
+            latitude: 52.3702157,
+            longitude: 4.89516789,
+            accuracy: 10
+        }
+    });
+    banners.smart = DFP.createView({
+        top: 20,
+        width: Ti.UI.FILL,
+        height: 50,
+        adUnitId: "/6236286/smartBanner"
+    });
+    banners.sizes = DFP.createView({
+        top: 20,
+        width: Ti.UI.FILL,
+        height: 250,
+        adUnitId: "/6236286/sizesBanner",
+        adSizes: [
+            {width: 320, height: 50},
+            {width: 320, height: 100},
+            {width: 300, height: 100},
+            {width: 300, height: 250}
+        ]
+    });
+
+    // Add event listeners
+    banners.default.addEventListener("receivead", doReceiveAd);
+    banners.smart.addEventListener("receivead", doReceiveAd);
+    banners.sizes.addEventListener("receivead", doReceiveAd);
+
+    // Add ads to view
+    $.defaultAd.add(banners.default);
+    $.smartAd.add(banners.smart);
+    $.sizesAd.add(banners.sizes);
 })(arguments[0]);
 
 //  Handle button events
@@ -27,9 +72,9 @@ function resetButton() {
 // Handle ad events
 function requestAd() {
     _.each(banners, function(banner, name) {
-        if($[banner]) {
+        if(banner) {
             Ti.API.info(TAG, "requesting ad for " + name);
-            $[banner].requestAd();
+            banner.requestAd();
         }
     });
 }
